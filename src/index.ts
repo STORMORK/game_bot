c17ee4299e004dc624ae1daa9c40dde70dba19ca
STORMORK <krupka0304@gmail.com>
2026-09-03 14:55:44 +0300

    Create src/index.ts

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+import { bot } from './bot';
+
+bot.launch();
+
+process.once('SIGINT', () => bot.stop('SIGINT'));
+process.once('SIGTERM', () => bot.stop('SIGTERM'));
+
+console.log('Bot started');
